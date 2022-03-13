102c4b1101e44fe706359b9f6565ea4f1c725f58
Lucas <lucas.taferdevs@gmail.com>
2022-03-13 18:29:10 -0300

    make it better

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require("dotenv").config;
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
@@ -11,12 +11,12 @@ app.use(cors());
 
 app.use("/", contactRoute);
 
-if(process.env.NODE_END === "production"){
-    app.use(express.static("client/build"))
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-    })
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
 }
 
-const port = process.env.PORT || 3001;
-app.listen(port, console.log(`Server listening to PORT ${port}.`));
+const port = process.env.PORT || 5000;
+app.listen(port, console.log(`server listing to port 5000 only`));
