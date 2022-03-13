7361c752ca5d5c1891bea67dc3f2ed9c50080430
Lucas <lucas.taferdevs@gmail.com>
2022-03-13 17:59:28 -0300

    update

--- server.js
+++ server.js
@@ -11,5 +11,12 @@ app.use(cors());
 
 app.use("/", contactRoute);
 
+if(process.env.NODE_END === "production"){
+    app.use(express.static("client/build"))
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    })
+}
+
 const port = process.env.PORT || 3001;
 app.listen(port, console.log(`Server listening to PORT ${port}.`));
